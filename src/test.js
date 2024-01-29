f7b26af21de620a830b5f77d32cfa6364ebeca2f
Srinidhi Sasidharan <srinidhisasidharan@Srinidhis-Air.lan>
2024-01-28 19:36:19 -0500

    home done

--- src/test.js
+++ src/test.js
@@ -2,16 +2,20 @@ import styles from './css/test.module.css'
 import { useState } from 'react';
 import Navbar from './Navbar';
 import ConstructionIcon from '@mui/icons-material/Construction';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import { useNavigate } from 'react-router-dom';
 export default function Test(){
+const nav = useNavigate()
 const [mouseDownAt, setMouseDownAt] = useState(0.0);
 const [prevPercentage, setPrevPercentage] = useState(0.0);
 const [percentage, setPercentage] = useState(0.0);
-const [nextPercent, setnextPercent] = useState(10.0);
+const [nextPercent, setnextPercent] = useState(10);
 const [trackStyle, setTrackStyle] = useState({
     transform: `translate(${nextPercent}%, -50%)`, 
+    transition: 'all 1s'
   });
   const [imageStyle, setImageStyle] = useState({
-    objectPosition: `${100 + nextPercent}% center`
+    objectPosition: `${100 }% center`
   ,  duration: 1200, fill: "forwards", 
   });
 const handleDown = (e) =>{
@@ -34,7 +38,7 @@ const handleMove = e =>{
     setPercentage(nextPercent)
     setTrackStyle({
         transform: `translate(${nextPercent}%, -50%)`
-       , 
+       ,  transition: 'all 1s'
        });
       
         setImageStyle({
@@ -48,6 +52,10 @@ const handleUp = () =>{
     setMouseDownAt(0)
     setPrevPercentage(percentage);
 }
+const handleGrovify = ()=>{
+    console.log("doubleclick")
+    window.open('https://grovify.nidhiworks.com','_blank')
+}
 
 return(
     <div className={styles['body']}>
@@ -60,21 +68,54 @@ return(
        </div>
         <div className={styles['name']}>SRINIDHI SASIDHARAN</div>
        
-       <div className={styles['punchline']}> Hover over to unfold the story behind each snap</div>
+       <div className={styles['punchline']}>Tap and hover over to unfold the story behind each snap</div>
        </div>
     <div  id= {styles['image-track']} style={trackStyle} onClick = {(e)=>handleDown(e)} onMouseMove={handleMove}
        onMouseUp={handleUp} >
         <div className={`${styles.image}`}>
-        <img className={`${styles.common} ${styles.grovify}`} style = {imageStyle} src="./pl2.png" draggable="false" />
+        <img className={`${styles.common} ${styles.grovify}`} style = {imageStyle} src="./pl2.png" draggable="false"  />
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div>
+             <div>Tap and slide to navigate</div> 
+             <OpenInNewIcon sx={{color:"silver"}}  /></div>
+        </div>
+        <div className={`${styles.image}`}>
+        <img className={`${styles.common} ${styles.ga}`} style = {imageStyle} src="./emb1.png" draggable="false" />
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div>
+             <div>Tap and slide to navigate</div>  <OpenInNewIcon sx={{color:"silver"}}  /></div>
+        </div>
+        <div className={`${styles.image}`}>
+        <img className={`${styles.common} ${styles.notes}`} style = {imageStyle} src="./note-img2.png" draggable="false" />
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div> <div>Tap and slide to navigate</div> <OpenInNewIcon sx={{color:"silver"}}  /></div>
+        </div>
+        <div className={`${styles.image}`}>
+        <img className={`${styles.common} ${styles.parking}`} style = {imageStyle} src="./reviews.png" draggable="false" />
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div> <div>Tap and slide to navigate</div> <OpenInNewIcon sx={{color:"silver"}}  /></div>
+        </div>
+        <div className={`${styles.image}`}>
+        <img className={`${styles.common} ${styles.amazon}`} style = {imageStyle} src="https://images.unsplash.com/photo-1548021682-1720ed403a5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" draggable="false" />
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div> <div>Tap and slide to navigate</div> <OpenInNewIcon sx={{color:"silver"}}  /></div>
+        </div>
+        <div className={`${styles.image}`}>
+        <img className={`${styles.common} ${styles.infosys}`} style = {imageStyle} src="https://images.unsplash.com/photo-1496753480864-3e588e0269b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80" draggable="false" />
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div><div>Tap and slide to navigate</div>  <OpenInNewIcon sx={{color:"silver"}}  /></div>
+        </div>
+        <div className={`${styles.image}`}>
+        <img className={`${styles.common} ${styles.rover}`} style = {imageStyle} src="https://images.unsplash.com/photo-1613346945084-35cccc812dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1759&q=80" draggable="false" />
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div><div>Tap and slide to navigate</div>  <OpenInNewIcon sx={{color:"silver"}}  /></div>
+        </div>
+        <div className={`${styles.image}`}>
+        <img className={`${styles.common} ${styles.dance}`} style = {imageStyle} src="https://images.unsplash.com/photo-1516681100942-77d8e7f9dd97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" draggable="false" /> 
+        <div className={styles['tooltip']} onDoubleClick={handleGrovify}>
+             <div>Double click to open</div><div>Tap and slide to navigate</div> <OpenInNewIcon sx={{color:"silver"}}  /></div>
         </div>
         
-        <img className={`${styles.image} ${styles.ga}`} style = {imageStyle} src="./emb1.png" draggable="false" />
-        <img className={`${styles.image} ${styles.notes}`} style = {imageStyle} src="./note-img2.png" draggable="false" />
-        <img className={`${styles.image} ${styles.parking}`} style = {imageStyle} src="./reviews.png" draggable="false" />
-        <img className={`${styles.image} ${styles.amazon}`} style = {imageStyle} src="https://images.unsplash.com/photo-1548021682-1720ed403a5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" draggable="false" />
-        <img className={`${styles.image} ${styles.infosys}`} style = {imageStyle} src="https://images.unsplash.com/photo-1496753480864-3e588e0269b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80" draggable="false" />
-        <img className={`${styles.image} ${styles.rover}`} style = {imageStyle} src="https://images.unsplash.com/photo-1613346945084-35cccc812dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1759&q=80" draggable="false" />
-        <img className={`${styles.image} ${styles.dance}`} style = {imageStyle} src="https://images.unsplash.com/photo-1516681100942-77d8e7f9dd97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" draggable="false" /> 
     </div>
     </div>
 )
